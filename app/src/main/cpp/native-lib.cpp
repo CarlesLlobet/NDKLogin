c6dc7c04fe46b2e32711d86f156f99b3b42efd55
Carles <carles.llobet@applus.com>
2018-07-11 11:48:44 +0200

    Versió amb compareStrings en NDK

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,21 +1,23 @@
 #include <jni.h>
-#include <string>
+#include <string.h>
+#include <android/log.h>
+#include <cstring>
 
-extern "C" JNIEXPORT jstring
-
-JNICALL
-Java_com_example_cllobet_ndklogin_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_example_cllobet_ndklogin_Domain_MainActivity_stringFromJNI(JNIEnv *env, jobject instance) {
+JNIEXPORT jboolean JNICALL
+Java_com_example_cllobet_ndklogin_UI_LoginActivity_comparePass(JNIEnv *env, jobject instance, jstring pass1, jstring pass2) {
+    const char *nativePass1 = (env)->GetStringUTFChars(pass1, 0);
+    const char *nativePass2 = (env)->GetStringUTFChars(pass2, 0);
+    __android_log_write(ANDROID_LOG_ERROR, "NDK Pass 1: ", nativePass1);
+    __android_log_write(ANDROID_LOG_ERROR, "NDK Pass 2: ", nativePass2);
 
-    // TODO: Ferla util
-    std::string hello = "Hello from C++";
+    jboolean res = false;
+    if (strcmp(nativePass1,nativePass2) == 0){
+        __android_log_write(ANDROID_LOG_ERROR, "NDK Compare: ", "Estoy dentro");
+        res = true;
+    }
 
-    return env->NewStringUTF(hello.c_str());
+    (env)->ReleaseStringUTFChars(pass1, nativePass1);
+    (env)->ReleaseStringUTFChars(pass2, nativePass2);
+    return res;
 }
--- app/src/main/java/com/example/cllobet/ndklogin/UI/LoginActivity.java
+++ app/src/main/java/com/example/cllobet/ndklogin/UI/LoginActivity.java
@@ -69,7 +69,7 @@ public class LoginActivity extends AppCompatActivity {
                     AesCbcWithIntegrity.CipherTextIvMac civ = new AesCbcWithIntegrity.CipherTextIvMac(civString);
                     String decPass = AesCbcWithIntegrity.decryptString(civ, key);
                     Log.d("[Login] DecryptedPass:", decPass.toString());
-                    if (decPass.equals(password)) {
+                    if (comparePass(decPass, password)) { //decPass.equals(password)
                         textError.setText("");
                         // Launch Dashboard Screen
                         Intent dashboard = new Intent(getApplicationContext(), HomeActivity.class);
@@ -131,7 +131,7 @@ public class LoginActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native boolean comparePass(String password1, String password2);
 
     @Override
     public void onBackPressed() {
